dd02c25a25ed524b1581a1f7b903f7c3dd3a9f8a
Mohammed Al Ameen <ameenmohammed2311@gmail.com>
2023-01-27 15:32:22 +0100

    refactor(auth-middleware): adds auth middleware on global level via app.module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,16 @@
-import { Module } from '@nestjs/common';
+import { MiddlewareConsumer, Module, RequestMethod } from '@nestjs/common';
 import { PostModule } from './post/post.module';
 import { UserModule } from './user/user.module';
+import { AuthMiddleware } from './middlewares';
 
 @Module({
   imports: [PostModule, UserModule],
 })
-export class AppModule {}
+export class AppModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer
+      .apply(AuthMiddleware)
+      .exclude({ path: '/post/', method: RequestMethod.GET })
+      .forRoutes('/user/profile/', '/post/');
+  }
+}
--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -1,16 +1,11 @@
-import { MiddlewareConsumer, Module } from '@nestjs/common';
+import { Module } from '@nestjs/common';
 import { PrismaService } from 'prisma/prisma.service';
 import UserDAO from './dao/user.dao';
 import { UserController } from './user.controller';
 import { UserService } from './user.service';
-import { AuthMiddleware } from 'src/middlewares';
 
 @Module({
   providers: [PrismaService, UserService, UserDAO],
   controllers: [UserController],
 })
-export class UserModule {
-  configure(consumer: MiddlewareConsumer) {
-    consumer.apply(AuthMiddleware).forRoutes('/user/profile/');
-  }
-}
+export class UserModule {}
